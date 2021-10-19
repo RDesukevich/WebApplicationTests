7fc2a320ac1c34675b48dd6d7ebbf8622c65a72f
Димка Ковалёв <dimas096912602@gmail.com>
2021-10-19 12:04:20 +0300

    Add Model Test

---
+++ WebApplicationTests/Models/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationTests.Models
+{
+    public class Test
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Guid SectionId { get; set; }
+        public Section Section { get; set; }
+        public List<Question> Questions = new List<Question>();
+    }
+}
