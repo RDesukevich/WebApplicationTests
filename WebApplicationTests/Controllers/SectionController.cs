1223761249f529f4d6dabd747a4d8700e047a9c5
Димка Ковалёв <dimas096912602@gmail.com>
2021-10-19 17:14:19 +0300

    Add Section Controller

---
+++ WebApplicationTests/Controllers/SectionController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationTests.Controllers
+{
+    public class SectionController : Controller
+    {
+        private readonly ISectionService _section;
+
+        public SectionController(ISectionService section)
+        {
+            _section = section;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_section.Get());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Section section)
+        {
+            await _section.Create(section);
+            return RedirectToAction("Index");
+        }
+    }
+}
