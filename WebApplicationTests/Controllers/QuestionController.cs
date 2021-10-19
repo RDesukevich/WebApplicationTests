9465b56abb6a3e078c710f44c3960069877b0051
Димка Ковалёв <dimas096912602@gmail.com>
2021-10-19 17:21:23 +0300

    Add Question Controller

---
+++ WebApplicationTests/Controllers/QuestionController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationTests.Controllers
+{
+    public class QuestionController : Controller
+    {
+        private readonly IQuestionService _question;
+
+        public QuestionController(IQuestionService question)
+        {
+            _question = question;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_question.Get());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Question question)
+        {
+            await _question.Create(question);
+            return RedirectToAction("Index");
+        }
+    }
+}
