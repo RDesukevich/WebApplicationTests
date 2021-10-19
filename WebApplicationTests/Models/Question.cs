fe4cd4dc8d3548d963890c87224e83109bfc742c
RDesukevich <owkdjfl>
2021-10-19 11:56:24 +0300

    Add model Question

---
+++ WebApplicationTests/Models/Question.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationTests.Models
+{
+    public class Question
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Guid TestId { get; set; }
+        public Test Test { get; set; }
+        public List<AnswerTheQuestion> Questions = new List<AnswerTheQuestion>();
+    }
+}
