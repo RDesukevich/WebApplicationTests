ea16978629d4c7aee760c56f0467471e7598a220
RDesukevich <owkdjfl>
2021-10-19 17:27:17 +0300

    Add service IQuestionService

---
+++ WebApplicationTests/Service/IQuestionService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationTests.Service
+{
+    public interface IQuestionService
+    {
+        public IEnumerable<Question> Get();
+        public Task<Question> Get(Guid questionId);
+        public Task Create(Question question);
+    }
+
+    public class QuestionService : IQuestionService
+    {
+        private readonly AppDb _db;
+
+        public QuestionService(AppDb db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Question> Get()
+        {
+            return _db.Questions;
+        }
+
+        public async Task<Question> Get(Guid questionId)
+        {
+            return await _db.Questions.FirstOrDefaultAsync(o => o.Id == questionId);
+        }
+
+        public async Task Create(Question question)
+        {
+            await _db.Questions.AddAsync(question);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
