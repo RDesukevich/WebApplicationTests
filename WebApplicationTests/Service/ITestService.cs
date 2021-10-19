f0460fc8d576b49e802e05f917326fb7b2e3090a
RDesukevich <owkdjfl>
2021-10-19 17:30:43 +0300

    Add service ITestService

---
+++ WebApplicationTests/Service/ITestService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationTests.Service
+{
+    public interface ITestService
+    {
+        public IEnumerable<Test> Get();
+        public Task<Test> Get(Guid testId);
+        public Task Create(Test test);
+    }
+
+    public class TestService : ITestService
+    {
+        private readonly AppDb _db;
+
+        public TestService(AppDb db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Test> Get()
+        {
+            return _db.Tests;
+        }
+
+        public async Task<Test> Get(Guid testId)
+        {
+            return await _db.Tests.FirstOrDefaultAsync(o => o.Id == testId);
+        }
+
+        public async Task Create(Test test)
+        {
+            await _db.Tests.AddAsync(test);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
