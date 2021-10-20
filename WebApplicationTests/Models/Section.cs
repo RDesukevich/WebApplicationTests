1d1cb685a8c4eefa0d373f1781384cd2080eb8aa
Димка Ковалёв <dimas096912602@gmail.com>
2021-10-20 18:16:48 +0300

    Fix Section

--- WebApplicationTests/Models/Section.cs
+++ WebApplicationTests/Models/Section.cs
@@ -7,5 +7,8 @@ namespace WebApplicationTests.Models
 {
     public class Section
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public List<Test> Tests { get; set; } = new List<Test>();
     }
 }
